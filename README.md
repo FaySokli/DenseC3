a2dfecb415481fd6f4b46f2f83c375b7b9f2f765
FaySokli <efsokli@gmail.com>
2024-11-12 13:25:24 +0000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DenseRetrievalMoE
+To reproduce the experiments complete the following steps:
+
+## Step 1:
+Run the following command to complete data pre-processing and bring all datasets to the BEIR format: <br>
+```python3 data_preprocessing.py```
+
+## Step 2:
+Update the parameters in the ```pipeline.sh``` file to the desired ones and execute the script. <br>
+For example, the following command will train SB_MoE on TinyBERT using the HotpotQA dataset by employing 6 experts: <br>
+```python3 1_train_new_moe.py model=tinybert dataset=hotpotqa testing=hotpotqa model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6```
--- src/pipeline.sh
+++ src/pipeline.sh
@@ -1,54 +1,39 @@
-DATASET=nfcorpus
-MODEL=contriever
+DATASET=computer_science
+MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-5 training.max_epoch=30 training.batch_size=128
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 
 
-DATASET=fever
-MODEL=contriever
+DATASET=political_science
+MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-4 training.max_epoch=10 training.batch_size=64
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
-
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-6 training.max_epoch=10 training.batch_size=64 model.adapters.use_adapters=False
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
-echo "$DATASET Zero"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 
 
 DATASET=hotpotqa
-MODEL=contriever
+MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-4 training.max_epoch=10 training.batch_size=64
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
-
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-6 training.max_epoch=10 training.batch_size=64 model.adapters.use_adapters=False
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
-echo "$DATASET Zero"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 
 
 DATASET=nq-train
-MODEL=contriever
+MODEL=tinybert
 
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-4 training.max_epoch=10 training.batch_size=64
+python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 TESTING_DATASET=$DATASET
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
+python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 echo "$DATASET MoE"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64
-
-python3 1_train_new_moe.py model=$MODEL dataset=$DATASET testing=$DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.lr=1e-6 training.max_epoch=10 training.batch_size=64 model.adapters.use_adapters=False
-python3 2_create_embedding_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
-echo "$DATASET Zero"
-python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET dataset.model_dir='output/'$DATASET'/saved_model_biencoder' training.batch_size=64 model.adapters.use_adapters=False
+python3 3_test_biencoder_moe.py model=$MODEL dataset=$TESTING_DATASET testing=$TESTING_DATASET model.adapters.use_adapters=True model.adapters.num_experts_to_use=6 model.adapters.num_experts=6
 
